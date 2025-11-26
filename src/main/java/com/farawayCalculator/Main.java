bfb89caaeed357e2ff90e183352c742277f176e3
FerandDalatieh <m.ferand.d@gmail.com>
2025-11-26 01:42:04 +0100

    add comments of code structure in main class

--- src/main/java/com/farawayCalculator/Main.java
+++ src/main/java/com/farawayCalculator/Main.java
@@ -3,6 +3,16 @@ package com.farawayCalculator;
 public class Main {
     public static void main (String[] args){
 
+        // read input (form a json file)
+
+        // initialize game, players and cards
+
+        // calculate score of each player
+
+        // set the winner based of the score
+
+        // output the score of each player and the winner
+
 
     }
 }
