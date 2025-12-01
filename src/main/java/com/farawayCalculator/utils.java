c2790730acdd219f1009bb949ec9ec415570763d
FerandDalatieh <m.ferand.d@gmail.com>
2025-12-01 01:39:03 +0100

    Feature: resolve issue #6 by implementing a logic to consider region cards 69-76 in the score

--- src/main/java/com/farawayCalculator/utils.java
+++ src/main/java/com/farawayCalculator/utils.java
@@ -7,4 +7,8 @@ public class utils {
         return Math.min(Math.min(a, b), Math.min(c, d));
     }
 
+    public static int min3(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
+
 }
